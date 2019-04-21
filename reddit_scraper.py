a48128a5b46a9581b075d076d3df618818b7cb87
Yuval Goldman1 <goldman1yuval@gmail.com>
2019-04-21 15:30:46 +0300

    ...

---
+++ reddit_scraper.py
@@ -0,0 +1,5 @@
+import praw
+import pandas
+
+
+reddit = praw.Reddit(user_agent='Comment Extraction (by /u/USERNAME)',client_id='**********',client_secret="***********",username='********', password='*******')
