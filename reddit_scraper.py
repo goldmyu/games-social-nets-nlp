52ea18fdf4edf9ba5b7cd14406cdd6e18a6427f7
Yuval Goldman1 <goldman1yuval@gmail.com>
2019-04-21 18:42:01 +0300

    ...

--- reddit_scraper.py
+++ reddit_scraper.py
@@ -1,5 +1,13 @@
 import praw
-import pandas
+import configparser
 
+config = configparser.ConfigParser()
+config.read("credentials.ini")
 
-reddit = praw.Reddit(user_agent='Comment Extraction (by /u/USERNAME)',client_id='**********',client_secret="***********",username='********', password='*******')
+reddit = praw.Reddit(client_id=config['REDDIT']['personal_use_script'],
+                     client_secret=config['REDDIT']['secret'],
+                     user_agent=config['REDDIT']['script_name'],
+                     username=config['REDDIT']['username'],
+                     password=config['REDDIT']['password'])
+
+print(reddit.user.me())
