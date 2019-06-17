6f91262ca6d3ebde51e01b3d33b3bd87728ea0ce
Yuval Goldman1 <goldman1yuval@gmail.com>
2019-06-17 17:51:03 +0300

    add data cleaning

---
+++ clean_data.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import re  # regular expression
+import preprocessor as p
+
+data = pd.read_csv("data-sets/fortnite.csv")
+# print(data['text'])
+
+# HappyEmoticons
+emoticons_happy = {':-)', ':)', ';)', ':o)', ':]', ':3', ':c)', ':>', '=]', '8)', '=)', ':}', ':^)', ':-D', ':D', '8-D',
+                   '8D', 'x-D', 'xD', 'X-D', 'XD', '=-D', '=D', '=-3', '=3', ':-))', ":'-)", ":')", ':', ':^', '>:P',
+                   ':-P', ':P', 'X-P', 'x-p', 'xp', 'XP', ':-p', ':p', '=p', ':-b', ':b', '>:)', '>;)', '>:-)', '<3'}
+
+# Sad Emoticons
+emoticons_sad = {':L', ':-/', '>:/', ':S', '>:[', ':@', ':-(', ':[', ':-||', '=L', ':<', ':-[', ':-<', '=\\', '=/',
+                 '>:(', ':(', '>.<', ":'-(", ":'(", ':\\', ':-c', ':c', ':{', '>:\\', ';('}
+
+# Emoji patterns
+emoji_pattern = re.compile("["
+                           u"\U0001F600-\U0001F64F"  # emoticons
+                           u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+                           u"\U0001F680-\U0001F6FF"  # transport & map symbols
+                           u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+                           u"\U00002702-\U000027B0"
+                           u"\U000024C2-\U0001F251"
+                           "]+", flags=re.UNICODE)
+# combine sad and happy emoticons
+emoticons = emoticons_happy.union(emoticons_sad)
+
+for i in range (data.shape[0]):
+    print("\n" + data['text'][i])
+    clean_text = p.clean(data['text'][i])
+    print(clean_text)
