81bb4a64daa6ef3bbe2c85f26c90145a136a1ca8
Yuval Goldman1 <goldman1yuval@gmail.com>
2019-06-18 14:17:45 +0300

    updating data cleaning

--- clean_data.py
+++ clean_data.py
@@ -1,9 +1,17 @@
 import pandas as pd
 import re  # regular expression
 import preprocessor as p
+from nltk.tokenize import word_tokenize
+import string
 
+# import nltk
+# nltk.download()
+
+# =================================================================================================
 data = pd.read_csv("data-sets/fortnite.csv")
-# print(data['text'])
+clean_data = data['text'][0:10000].copy()
+
+# =================================================================================================
 
 # HappyEmoticons
 emoticons_happy = {':-)', ':)', ';)', ':o)', ':]', ':3', ':c)', ':>', '=]', '8)', '=)', ':}', ':^)', ':-D', ':D', '8-D',
@@ -26,7 +34,46 @@ emoji_pattern = re.compile("["
 # combine sad and happy emoticons
 emoticons = emoticons_happy.union(emoticons_sad)
 
-for i in range (data.shape[0]):
-    print("\n" + data['text'][i])
-    clean_text = p.clean(data['text'][i])
-    print(clean_text)
+
+def clean_tweets(tweet):
+    # after tweepy preprocessing the colon symbol left remain after      #removing mentions
+    tweet = re.sub(r':', '', tweet)
+    tweet = re.sub(r'‚Ä¶', '', tweet)
+    tweet = re.sub(r'\'', '', tweet)
+    tweet = re.sub(r'’', '', tweet)
+    tweet = re.sub(r'\\"', '', tweet)
+    tweet = re.sub(r'\.', '', tweet)
+    tweet = re.sub(r'…', '', tweet)
+    tweet = re.sub(r'\*', '', tweet)
+
+    # replace consecutive non-ASCII characters with a space
+    tweet = re.sub(r'[^\x00-\x7F]+', ' ', tweet)
+
+    # remove emojis from tweet
+    tweet = emoji_pattern.sub(r'', tweet)
+    # filter using NLTK library append it to a string
+    filtered_tweet = []
+
+    # looping through conditions
+    word_tokens = word_tokenize(tweet)
+    for w in word_tokens:
+        # check tokens against stop words , emoticons and punctuations
+        if w not in emoticons and w not in string.punctuation:
+            filtered_tweet.append(w)
+    return ' '.join(filtered_tweet)
+    # print(word_tokens)
+    # print(filtered_sentence)return tweet
+
+
+p.set_options(p.OPT.URL, p.OPT.MENTION, p.OPT.EMOJI, p.OPT.SMILEY, p.OPT.RESERVED)
+
+for i in range(10000):
+    print(data['text'][i])
+    clean_data[i] = p.clean(data['text'][i])
+    clean_data[i] = clean_tweets(clean_data[i])
+    print(clean_data[i])
+    # print("\n")
+    # if i%10000==0:
+    #     print("iteration {}".format(i))
+
+clean_data.to_csv("data-sets/clean_fortnite_data.csv", index=False)
