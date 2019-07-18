4ccc853f2416683f758ad267eae4bb933c7c5656
adarOv <adarov@post.bgu.ac.il>
2019-07-18 15:36:52 +0300

    sentiment analysis and bag of words

--- models/SentimentAnalysis.py
+++ models/SentimentAnalysis.py
@@ -2,28 +2,54 @@ from textblob import TextBlob
 import numpy as np
 import pandas as pd
 
-path = 'data-sets/'
-file_name = 'clean-full-fortnite-no-duplicates.csv'
-data = pd.read_csv(path+file_name)
-results = []
+path = '../data-sets/'
+name =''# 'clean_'
+files = ['fortnite_clean', 'clean-full-fortnite-no-duplicates']
 
-polarity_sum = 0
-subjectivity_sum = 0
 
-for twite in data.values:
-    if type(twite[0]) is str:
-        testimonial = TextBlob(twite[0])
-        polarity_sum += testimonial.sentiment.polarity
-        subjectivity_sum += testimonial.sentiment.subjectivity
-        results.append([twite[0], testimonial.sentiment.polarity, testimonial.sentiment.subjectivity])
+def calc(file_name):
+    data = pd.read_csv(path+file_name)
 
-df = pd.DataFrame(data=np.array(results), columns=['twite','polarity', 'subjectivity'])
-df.to_csv(path+'Results/Sentiment/'+file_name)
+    polarity_sum = 0
+    subjectivity_sum = 0
+    positive_polarity_sum = 0
+    negative_polarity_sum = 0
+    positive_polarity_num = 0
+    negative_polarity_num = 0
+    neutral_polarity_num = 0
 
-# average
-polarity_avg = polarity_sum / len(results)
-subjectivity_avg = subjectivity_sum / len(results)
-array = [[polarity_avg, subjectivity_avg, len(results)]]
+    for row in data.values:
+        if type(row[0]) is str:
+            testimonial = TextBlob(row[0])
+            polarity_sum += testimonial.sentiment.polarity
+            subjectivity_sum += testimonial.sentiment.subjectivity
+            # results.append([twite[0], testimonial.sentiment.polarity, testimonial.sentiment.subjectivity])
+            # case polarity is positive
+            if testimonial.sentiment.polarity > 0:
+                positive_polarity_sum += testimonial.sentiment.polarity
+                positive_polarity_num += 1
+            # case polarity is negative
+            elif testimonial.sentiment.polarity < 0:
+                negative_polarity_sum += testimonial.sentiment.polarity
+                negative_polarity_num += 1
+            else:
+                neutral_polarity_num += 1
 
-df_avg = pd.DataFrame(data=np.array(array), columns=['polarity_avg', 'subjectivity_avg', 'num_of_twites'])
-df_avg.to_csv(path+'Results/Sentiment_Average/'+file_name)
+
+    # df = pd.DataFrame(data=np.array(results), columns=['twite','polarity', 'subjectivity'])
+    # df.to_csv(path+'Results/Sentiment/'+file_name)
+
+    # average
+    num_of_rows = positive_polarity_num + negative_polarity_num + neutral_polarity_num
+    polarity_avg = polarity_sum / num_of_rows
+    subjectivity_avg = subjectivity_sum / num_of_rows
+    positive_polarity_avg = positive_polarity_sum / positive_polarity_num
+    negative_polarity_avg = negative_polarity_sum / negative_polarity_num
+    array = [[positive_polarity_avg, negative_polarity_avg, positive_polarity_num, negative_polarity_num, neutral_polarity_num, polarity_avg, subjectivity_avg, num_of_rows]]
+
+    df_avg = pd.DataFrame(data=np.array(array), columns=['positive pol avg', 'negative pol avg', 'num of positive polarity', 'num of negative polarity','num of neutral polarity','polarity_avg', 'subjectivity_avg', 'num_of_rows'])
+    df_avg.to_csv(path+'Results/Sentiment_Average/'+file_name)
+
+
+for file in files:
+    calc(name + file + '.csv')
--- models/bags_of_words.py
+++ models/bags_of_words.py
@@ -2,13 +2,19 @@
 # nltk.download()
 import nltk
 from nltk.tokenize import word_tokenize
+import numpy as np
 from collections import Counter
 import pandas as pd
+import csv
 from pandas import DataFrame
+import operator
 from nltk.corpus import stopwords
+import collections
 
-DATA_SETS_PATH = "data-sets/fortnite_clean.csv"
-data = pd.read_csv(DATA_SETS_PATH)
+game = 'wow'
+DATA_SETS_PATH = '../data-sets/'
+name_file = 'clean_no_stop_words_'+game+'.csv'
+data = pd.read_csv(DATA_SETS_PATH+name_file)
 df = DataFrame(data)
 row_list = df.values.tolist()
 words_list = []
@@ -19,5 +25,15 @@ for row in (row_list):
         words_list.extend(list)
 
 c = Counter(words_list)
-print(c.most_common(1000))
+sum = sum(c.values())
+dict_pair = dict(c)
+sorted_x = sorted(dict_pair.items(), key=operator.itemgetter(1), reverse=True)
+dict_pair = collections.OrderedDict(sorted_x)
+
+with open(DATA_SETS_PATH+'Results/Frequents/'+name_file, 'w') as csv_file:
+    writer = csv.writer(csv_file)
+    for key, value in dict_pair.items():
+       writer.writerow([key, value, value/sum])
+
+
 print("f")
