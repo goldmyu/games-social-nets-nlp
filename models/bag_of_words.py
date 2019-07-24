1a5fee7420f105f10438d59455fd201f760e042f
Yuval Goldman1 <goldman1yuval@gmail.com>
2019-07-24 19:44:06 +0300

    refactor

--- models/bag_of_words.py
+++ models/bag_of_words.py
@@ -3,30 +3,46 @@ import pandas as pd
 import csv
 import operator
 import collections
-
-game_name = 'pubg'
-DATA_SETS_PATH = '../data-sets/cleaned-data-sets/'
-name_file = 'clean_no_stop_words_' + game_name + '.csv'
-df = pd.read_csv(DATA_SETS_PATH+name_file)
-row_list = df.values.tolist()
-words_list = []
-
-for row in (row_list):
-    if type(row[0]) is str:
-        list = row[0].split()
-        words_list.extend(list)
-
-c = Counter(words_list)
-sum = sum(c.values())
-dict_pair = dict(c)
-sorted_x = sorted(dict_pair.items(), key=operator.itemgetter(1), reverse=True)
-dict_pair = collections.OrderedDict(sorted_x)
+from WordsSets.SexismRacismTrumpWords import *
 
 
-with open(DATA_SETS_PATH +'Results/Frequents_words_' + game_name, 'w') as csv_file:
-    writer = csv.writer(csv_file)
-    for key, value in dict_pair.items():
-       writer.writerow([key, value, value/sum])
+datasets_path = '../data-sets/'
+file_name_prefix = 'clean_no_stop_words_'
 
+games_list = {
+    'fortnite': pd.read_csv(datasets_path + file_name_prefix + 'fortnite.csv'),
+    'pubg': pd.read_csv(datasets_path + file_name_prefix + 'pubg.csv'),
+    'fifa': pd.read_csv(datasets_path + file_name_prefix + 'fifa.csv'),
+    'minecraft': pd.read_csv(datasets_path + file_name_prefix + 'minecraft.csv'),
+    'bloodstained': pd.read_csv(datasets_path + file_name_prefix + 'bloodstained.csv'),
+    'lol': pd.read_csv(datasets_path + file_name_prefix + 'lol.csv'),
+    'overwatch': pd.read_csv(datasets_path + file_name_prefix + 'overwatch.csv'),
+    'sims': pd.read_csv(datasets_path + file_name_prefix + 'sims.csv'),
+    'wow': pd.read_csv(datasets_path + file_name_prefix + 'wow.csv'),
+    'dota2': pd.read_csv(datasets_path + file_name_prefix + 'dota2.csv'),
+}
 
-print("Finished calculating frequent words for the game : " + game_name)
+for game_name, game_df in games_list.items():
+    row_list = game_df.values.tolist()
+    words_list = []
+
+    for row in row_list:
+        if type(row[0]) is str:
+            _list = row[0].split()
+            words_list.extend(_list)
+
+    c = Counter(words_list)
+    total_num_of_words = sum(c.values())
+    dict_pair = dict(c)
+    sorted_x = sorted(dict_pair.items(), key=operator.itemgetter(1), reverse=True)
+    dict_pair = collections.OrderedDict(sorted_x)
+
+    with open(datasets_path + '/bag_of_words/negative_words_freq_' + game_name, 'w') as csv_file:
+        writer = csv.writer(csv_file)
+        for word, apperence_num in dict_pair.items():
+            for attribute, neg_words in lists_of_negative_words.items():
+                if word in neg_words:
+                    writer.writerow([word, apperence_num, apperence_num / total_num_of_words])
+                    break
+
+    print("Finished calculating frequent words for the game : " + game_name)
