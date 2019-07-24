7f4a353c6dc99402f1005412d0bb42fbb0fd06a3
juliatul <julia.tulisov@gmail.com>
2019-07-25 00:35:02 +0300

    refactor

--- WordsSets/SexismRacismTrumpWords.py
+++ WordsSets/SexismRacismTrumpWords.py
@@ -2,12 +2,14 @@ lists_of_negative_words = {
 
     'sexism_words': ['slut', 'sluts', 'frigid', 'tease', 'teaser', 'cocktease', 'prude', 'whore', 'whores', 'bitch',
                       'bitchy','bitches', 'bitchass', 'milf', 'milfs', 'nag', 'hag', 'bimbo', 'sexygirlz', 'waman',
-                      'hormonal', 'queer', 'sexy' ,'hotass', 'icequeen', 'thicc'],
+                      'hormonal', 'queer', 'sexy' ,'hotass', 'icequeen', 'thicc', 'sexualizing', 'sexualized',
+                     'objectifying', 'sexist'],
     # 22
 
     'racism_words': ['nigga', 'niggas', 'nigger', 'nazi', 'nazis', 'neonazi', 'neonazis', 'gaynigga', 'monkeynigger',
                      'slave', 'slaves', 'slavery', 'racism', 'racist', 'racists', 'genocide', 'apartheid', 'xenophobia',
-                     'guido', 'hillbilly', 'islamophobic', 'islamophobe'],
+                     'guido', 'hillbilly', 'islamophobic', 'islamophobe','terrorists', 'terrorist','concentration',
+                     'concentrationcamps'],
     # 22
 
     'trump-hate_words' : ['drumpf', 'drumpff', 'meathead', 'orangutan', 'trumpster', 'trumpconcentrationcamps',
@@ -20,22 +22,24 @@ lists_of_negative_words = {
     'bad_words' : ['shit', 'fuck', 'fucking', 'fucked', 'fucks','fuckers', 'fucker', 'retard', 'retarded', 'asshole', 'assholes', 'hate',
                  'ass', 'asses', 'die', 'enemy', 'suck', 'sucks', 'toxic', 'dumb', 'terrible', 'bullshit', 'dick', 'evil', 'ugly',
                  'idiot', 'idiots', 'dirty', 'disgusting', 'pussy', 'dumbass', 'pathetic', 'cunt', 'hitler', 'molester',
-                   'donkey', 'rat','harass', 'stupid','shithole', 'motherfuckers','traitorsx', 'fuckme','sexaddict'],
+                   'donkey', 'rat','harass', 'stupid','shithole', 'motherfuckers','traitors', 'traitor', 'fuckme','sexaddict',
+                   'insults','cyka','suka','creepy','abusive','penis','hateful','disgraced'],
 
 }
 
 
 lists_of_neutral_words = {
     'sexism_neutral_words': ['she', 'girl', 'girls', 'woman', 'womans', 'women', 'womens', 'sister', 'sisters',
-                             'girlfriend', 'women', 'wife', 'female', 'her', 'hers', 'mother', 'aunt', 'daughter',
-                             'daughters', 'goddaughter', 'granddaughter', 'mom'],
+                             'girlfriend', 'women', 'wife', 'female', 'females', 'her', 'hers', 'mother', 'aunt', 'daughter',
+                             'daughters', 'goddaughter', 'granddaughter', 'mom','moms', 'niece','mrs', 'sis', 'egirl','ladies',
+                             'lady', 'mum', 'gf'],
 
     'racism_neutral_words': ['jew', 'jews', 'jewish', 'asian', 'asians', 'arab', 'arabic', 'arabs', 'african',
                            'africans', 'southafrican', 'africanamerican', 'caucasian', 'american', 'americans',
                            'nonamerican', 'latino', 'hispanic', 'hispanics', 'mexican', 'mexicans', 'islam',
-                           'islamic', 'muslim', 'muslims'],
+                           'islamic', 'muslim', 'muslims','refugees','russian','russians','egyptians','egyptian'],
 
-    'trump-hate_neutral_words': ['trump', 'donaldtrump', 'president', 'realdonaldtrump', 'donaldjtrumpthpresident'],
+    'trump-hate_neutral_words': ['trump', 'donaldtrump', 'president', 'realdonaldtrump', 'donaldjtrumpthpresident', 'republican', 'republicans'],
 }
 
 
