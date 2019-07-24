684988e6ab8a47efa2468f464a0c94ce023c7963
Yuval Goldman1 <goldman1yuval@gmail.com>
2019-07-24 17:10:11 +0300

    refactor

--- WordsSets/SexismRacismTrumpWords.py
+++ WordsSets/SexismRacismTrumpWords.py
@@ -1,37 +1,45 @@
-sexists_words = [ 'slut', 'sluts', 'frigid', 'tease', 'teaser', 'cocktease', 'prude', 'whore', 'whores', 'bitch', 'bitchy',
-                  'bitches', 'bitchass',  'milf', 'milfs', 'nag', 'hag', 'bimbo', 'sexygirlz', 'waman', 'hormonal', 'queer']
-# 22
+lists_of_negative_words = {
 
-neutral_female_words = ['she', 'girl', 'girls', 'woman', 'womans', 'women', 'womens',  'sister', 'sisters',  'girlfriend',
-                        'women', 'wife', 'female', 'her', 'hers', 'mother', 'aunt', 'daughter', 'daughters',
-                        'goddaughter', 'granddaughter', 'mom']
+    'sexists_words': ['slut', 'sluts', 'frigid', 'tease', 'teaser', 'cocktease', 'prude', 'whore', 'whores', 'bitch',
+                      'bitchy',
+                      'bitches', 'bitchass', 'milf', 'milfs', 'nag', 'hag', 'bimbo', 'sexygirlz', 'waman', 'hormonal',
+                      'queer'],
+    # 22
 
+    'racist_words': ['nigga', 'niggas', 'nigger', 'nazi', 'nazis', 'neonazi', 'neonazis', 'gaynigga', 'monkeynigger',
+                     'slave',
+                     'slaves', 'slavery', 'racism', 'racist', 'racists', 'genocide', 'apartheid', 'xenophobia', 'guido',
+                     'hillbilly', 'islamophobic', 'islamophobe'],
+    # 22
 
-racist_words =['nigga', 'niggas', 'nigger', 'nazi', 'nazis', 'neonazi', 'neonazis',  'gaynigga', 'monkeynigger', 'slave',
-               'slaves', 'slavery', 'racism', 'racist', 'racists',  'genocide', 'apartheid', 'xenophobia', 'guido',
-               'hillbilly','islamophobic', 'islamophobe']
-# 22
+    'trump_hate': ['drumpf', 'drumpff', 'meathead', 'orangutan', 'trumpster', 'trumpconcentrationcamps',
+                   'mattersfucktrump',
+                   'antitrump', 'notrump', 'adiostrump'],
+    # 10
 
+    'bad_words': ['shit', 'fuck', 'fucking', 'fucked', 'fucks', 'retard', 'retarded', 'asshole', 'hate', 'ass',
+                  'die', 'enemy', 'suck', 'sucks', 'toxic', 'dumb', 'terrible', 'bullshit', 'dick', 'evil', 'ugly',
+                  'idiot',
+                  'idiots', 'dirty', 'disgusting', 'pussy', 'dumbass', 'pathetic'],
 
-neutral_race_words= ['jew', 'jews', 'jewish',  'asian', 'asians',  'arab', 'arabic', 'arabs', 'african', 'africans',
-                     'southafrican', 'africanamerican', 'caucasian', 'american', 'americans', 'nonamerican', 'latino',
-                     'hispanic', 'hispanics', 'mexican', 'mexicans', 'islam', 'islamic',  'muslim', 'muslims']
-
-trump_hate =[ 'drumpf','drumpff',  'meathead', 'orangutan', 'trumpster','trumpconcentrationcamps' , 'mattersfucktrump',
-              'antitrump', 'notrump' , 'adiostrump']
-# 10
-
-trump_neutral=['trump', 'donaldtrump', 'president', 'realdonaldtrump', 'donaldjtrumpthpresident']
-
-
-
-maybe_Trump=['bumbledore', 'chickenhawk', 'taxevader','trumpofdoom', 'trumpenstein', 'trumpinator', 'trumpocalypse',
-             'trumptastrophe', 'adolftwitler']
+}
 
 
+lists_of_neutral_words = {
+    'neutral_female_words': ['she', 'girl', 'girls', 'woman', 'womans', 'women', 'womens', 'sister', 'sisters',
+                             'girlfriend',
+                             'women', 'wife', 'female', 'her', 'hers', 'mother', 'aunt', 'daughter', 'daughters',
+                             'goddaughter', 'granddaughter', 'mom'],
 
-bad_words =['shit', 'fuck' , 'fucking', 'fucked', 'fucks',  'retard', 'retarded',  'asshole', 'hate', 'ass',
-            'die', 'enemy', 'suck', 'sucks', 'toxic', 'dumb', 'terrible', 'bullshit', 'dick', 'evil', 'ugly', 'idiot',
-            'idiots', 'dirty', 'disgusting', 'pussy', 'dumbass', 'pathetic'  ]
+    'neutral_race_words': ['jew', 'jews', 'jewish', 'asian', 'asians', 'arab', 'arabic', 'arabs', 'african', 'africans',
+                           'southafrican', 'africanamerican', 'caucasian', 'american', 'americans', 'nonamerican',
+                           'latino',
+                           'hispanic', 'hispanics', 'mexican', 'mexicans', 'islam', 'islamic', 'muslim', 'muslims'],
+
+    'trump_neutral': ['trump', 'donaldtrump', 'president', 'realdonaldtrump', 'donaldjtrumpthpresident'],
+}
 
 
+
+# maybe_Trump = ['bumbledore', 'chickenhawk', 'taxevader', 'trumpofdoom', 'trumpenstein', 'trumpinator', 'trumpocalypse',
+#                'trumptastrophe', 'adolftwitler']
