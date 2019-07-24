903d0bdd2182dfbaf2d55d8f39c24b495ace4e53
Yuval Goldman1 <goldman1yuval@gmail.com>
2019-07-24 16:55:52 +0300

    refactor

--- models/embedding_examples.py
+++ models/embedding_examples.py
@@ -18,33 +18,30 @@ stop_words = set(stopwords.words('english'))
 # ======================================================================================================================
 
 games_models_list = {
-                    'fortnite': Word2Vec.load("../saved_embedding_models/fortnite.model"),
-                    'pubg': Word2Vec.load("../saved_embedding_models/pubg.model"),
-                    'fifa': Word2Vec.load("../saved_embedding_models/fifa.model"),
-                    'minecraft': Word2Vec.load("../saved_embedding_models/minecraft.model"),
-                    'bloodstained': Word2Vec.load("../saved_embedding_models/bloodstained.model"),
-                    'lol': Word2Vec.load("../saved_embedding_models/lol.model"),
-                    'overwatch': Word2Vec.load("../saved_embedding_models/overwatch.model"),
-                    'sims': Word2Vec.load("../saved_embedding_models/sims.model"),
-                    'wow': Word2Vec.load("../saved_embedding_models/wow.model"),
-                     }
-
-
-
-
+    'fortnite': Word2Vec.load("../saved_embedding_models/fortnite.model"),
+    'pubg': Word2Vec.load("../saved_embedding_models/pubg.model"),
+    'fifa': Word2Vec.load("../saved_embedding_models/fifa.model"),
+    'minecraft': Word2Vec.load("../saved_embedding_models/minecraft.model"),
+    'bloodstained': Word2Vec.load("../saved_embedding_models/bloodstained.model"),
+    'lol': Word2Vec.load("../saved_embedding_models/lol.model"),
+    'overwatch': Word2Vec.load("../saved_embedding_models/overwatch.model"),
+    'sims': Word2Vec.load("../saved_embedding_models/sims.model"),
+    'wow': Word2Vec.load("../saved_embedding_models/wow.model"),
+    'dota2': Word2Vec.load("../saved_embedding_models/dota2.model"),
+}
 
 # ======================================================================================================================
 
-word_triplets_general = [['man', 'woman', 'gamer'],  # gamergirl - man is to woman as gamer is to gamergirl
+word_triplets = [['man', 'woman', 'gamer'],  # gamergirl - man is to woman as gamer is to gamergirl
                          ['woman', 'bitch', 'man'],  # bitch, slut, crazy
                          ['man', 'bro', 'woman'],  # bitch
                          ['man', 'nigga', 'woman'],
                          ['man', 'winner', 'woman'],
                          ['man', 'woman', 'pro'],
-                         ['man','pro','woman'],
+                         ['man', 'pro', 'woman'],
                          ['gamer', 'pro', 'woman'],
                          ['man', 'asshole', 'woman'],
-                         [ 'man','dude', 'woman'],
+                         ['man', 'dude', 'woman'],
                          ['bro', 'dude', 'sister'],
                          ['gamer', 'game', 'woman'],
                          ['man', 'streamer', 'girl'],
@@ -60,16 +57,16 @@ word_triplets_general = [['man', 'woman', 'gamer'],  # gamergirl - man is to wom
                          ['gamer', 'fortnite', 'trump']
                          ]
 
-trump_hate =['trump','boldfinger', 'bratman', 'bumbledore', 'chickenhawk', 'taxevader', 'drumpf', 'meathead', 'tictacdough', 'trumpofdoom', 'trumpelthinskin', 'trumpenstein', 'trumpinator', 'trumpocalypse', 'trumptastrophe',
-             'trumpkopf', 'groepenfuehrer', 'orangutan', 'trumpamaniac', 'trumpster', 'adolftwitler']
+trump_hate = ['trump', 'boldfinger', 'bratman', 'bumbledore', 'chickenhawk', 'taxevader', 'drumpf', 'meathead',
+              'tictacdough', 'trumpofdoom', 'trumpelthinskin', 'trumpenstein', 'trumpinator', 'trumpocalypse',
+              'trumptastrophe',
+              'trumpkopf', 'groepenfuehrer', 'orangutan', 'trumpamaniac', 'trumpster', 'adolftwitler']
 
 
-
-def run_triplet_examples(model,word_triplets_list):
-    print('These are interesting embedding examples from the game : ' + model)
-    for words_triples in word_triplets_list:
+def run_triplet_examples(model, _word_triplets):
+    for words_triples in _word_triplets:
         most_similar = [x[0] for x in model.wv.most_similar_cosmul(positive=[words_triples[1], words_triples[2]],
-                                                                            negative=[words_triples[0]])]
+                                                                   negative=[words_triples[0]])]
         print("\n{} is to {} like {} is to: \n{}".format(
             words_triples[0], words_triples[1], words_triples[2], most_similar))
 
@@ -81,10 +78,15 @@ def run_similar_examples(model, words):
         except:
             print("word {} is not in vocab".format(word))
 
+
 # run_triplet_examples(word_triplets_general)
 
 
 for game_name, game_w2v_model in games_models_list.items():
-    run_similar_examples(game_name,game_w2v_model.value, trump_hate)
-
-
+    try:
+        print('Most similar words from the game : ' + game_name)
+        run_similar_examples(game_w2v_model, trump_hate)
+        print('Most intersting triplet examples for the game : ' + game_name)
+        run_triplet_examples(game_w2v_model, word_triplets)
+    except Exception as e:
+        print("For game {}, got the following error:\n{}".format(game_name,str(e)))
