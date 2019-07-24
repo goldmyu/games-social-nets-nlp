03e575ad22a2304c3395dd49172177412ea9d18c
Yuval Goldman1 <goldman1yuval@gmail.com>
2019-07-24 17:22:59 +0300

    refactor

--- WordsSets/SexismRacismTrumpWords.py
+++ WordsSets/SexismRacismTrumpWords.py
@@ -1,40 +1,35 @@
 lists_of_negative_words = {
 
     'sexists_words': ['slut', 'sluts', 'frigid', 'tease', 'teaser', 'cocktease', 'prude', 'whore', 'whores', 'bitch',
-                      'bitchy',
-                      'bitches', 'bitchass', 'milf', 'milfs', 'nag', 'hag', 'bimbo', 'sexygirlz', 'waman', 'hormonal',
-                      'queer'],
+                      'bitchy','bitches', 'bitchass', 'milf', 'milfs', 'nag', 'hag', 'bimbo', 'sexygirlz', 'waman',
+                      'hormonal', 'queer'],
     # 22
 
     'racist_words': ['nigga', 'niggas', 'nigger', 'nazi', 'nazis', 'neonazi', 'neonazis', 'gaynigga', 'monkeynigger',
-                     'slave',
-                     'slaves', 'slavery', 'racism', 'racist', 'racists', 'genocide', 'apartheid', 'xenophobia', 'guido',
-                     'hillbilly', 'islamophobic', 'islamophobe'],
+                     'slave', 'slaves', 'slavery', 'racism', 'racist', 'racists', 'genocide', 'apartheid', 'xenophobia',
+                     'guido', 'hillbilly', 'islamophobic', 'islamophobe'],
     # 22
 
     'trump_hate': ['drumpf', 'drumpff', 'meathead', 'orangutan', 'trumpster', 'trumpconcentrationcamps',
-                   'mattersfucktrump',
-                   'antitrump', 'notrump', 'adiostrump'],
+                   'mattersfucktrump', 'antitrump', 'notrump', 'adiostrump'],
     # 10
 
     'bad_words': ['shit', 'fuck', 'fucking', 'fucked', 'fucks', 'retard', 'retarded', 'asshole', 'hate', 'ass',
                   'die', 'enemy', 'suck', 'sucks', 'toxic', 'dumb', 'terrible', 'bullshit', 'dick', 'evil', 'ugly',
-                  'idiot',
-                  'idiots', 'dirty', 'disgusting', 'pussy', 'dumbass', 'pathetic'],
+                  'idiot', 'idiots', 'dirty', 'disgusting', 'pussy', 'dumbass', 'pathetic'],
 
 }
 
 
 lists_of_neutral_words = {
     'neutral_female_words': ['she', 'girl', 'girls', 'woman', 'womans', 'women', 'womens', 'sister', 'sisters',
-                             'girlfriend',
-                             'women', 'wife', 'female', 'her', 'hers', 'mother', 'aunt', 'daughter', 'daughters',
-                             'goddaughter', 'granddaughter', 'mom'],
+                             'girlfriend', 'women', 'wife', 'female', 'her', 'hers', 'mother', 'aunt', 'daughter',
+                             'daughters', 'goddaughter', 'granddaughter', 'mom'],
 
-    'neutral_race_words': ['jew', 'jews', 'jewish', 'asian', 'asians', 'arab', 'arabic', 'arabs', 'african', 'africans',
-                           'southafrican', 'africanamerican', 'caucasian', 'american', 'americans', 'nonamerican',
-                           'latino',
-                           'hispanic', 'hispanics', 'mexican', 'mexicans', 'islam', 'islamic', 'muslim', 'muslims'],
+    'neutral_race_words': ['jew', 'jews', 'jewish', 'asian', 'asians', 'arab', 'arabic', 'arabs', 'african',
+                           'africans', 'southafrican', 'africanamerican', 'caucasian', 'american', 'americans',
+                           'nonamerican', 'latino', 'hispanic', 'hispanics', 'mexican', 'mexicans', 'islam',
+                           'islamic', 'muslim', 'muslims'],
 
     'trump_neutral': ['trump', 'donaldtrump', 'president', 'realdonaldtrump', 'donaldjtrumpthpresident'],
 }
